20ad500bcb95886b7837f06f2c6c1e279a211f84
seanyour <2697968649@qq.com>
2023-01-10 17:41:14 +0800

    add dependencies

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+    <template #extra>
+      <a-button type="primary" @click="router.push('/example/')">Back Home</a-button>
+    </template>
+  </a-result>
+</template>
+
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+</script>
+
+<style scoped>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,40 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from "path";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
+function pathResolve (dir:string){
+  return resolve(__dirname, dir);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/example/',
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [
+            AntDesignVueResolver()
+        ]
+      }),
+      Components({
+        resolvers: [
+            AntDesignVueResolver()
+        ]
+      })
+  ],
   resolve: {
+    extensions: ['.js','.ts','.vue'],
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'assets': pathResolve('./src/assets'),
+      'components': pathResolve('./src/components'),
+      'router': pathResolve('./src/router'),
+      'stores': pathResolve('./src/stores'),
+      'views': pathResolve('./src/views')
     }
   }
 })
